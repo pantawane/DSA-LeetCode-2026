674ca891517dc408281f79e5025abe13a3db3637
VANSH MANOJ PANTAWANE <pantawanevansh75@gmail.com>
2026-06-09 15:25:50 +0530

    Time: 4 ms (4.93%), Space: 17.7 MB (49.5%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
 
-    int solve(vector<int>& cost, int n, vector<int> &dp){
-        //base case
-        if (n == 0)
-        return cost[0];
+    int solve(vector<int>& cost, int n){
 
-        if (n == 1)
-        return cost[1];
-        
+        //step 1
+        vector<int> dp(n+1);
+        // step -2 base case
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
         // step 3
-        if(dp[n] != -1)
-        return dp[n];
+        // if(dp[n] != -1)
+        // return dp[n];
 
-        //step 2
-        dp[n] = cost[n] + min(solve(cost, n-1, dp), solve(cost, n-2, dp));
-        return dp[n];
+        //step 3
+
+        for(int i = 2; i < n ; i++){
+            dp[i] = cost[i] +  min(dp[i-1], dp[i-2]);
+        }
+           return min(dp[n-1], dp[n-2]);
     }
 
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
         // step 1
-        vector<int> dp(n+1, -1);
-        int ans = min(solve(cost, n-1, dp), solve(cost, n-2, dp));
-        return ans;
+        //vector<int> dp(n+1, -1);
+        //int ans = min(solve(cost, n-1, dp), solve(cost, n-2, dp));
+        return solve(cost, n);
     }
 };
