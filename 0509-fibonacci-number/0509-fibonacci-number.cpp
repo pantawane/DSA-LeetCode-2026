09fbe55e70493d226dc644e1fc3768968317e1a6
VANSH MANOJ PANTAWANE <pantawanevansh75@gmail.com>
2026-06-09 13:28:43 +0530

    Time: 2 ms (77.28%), Space: 8.3 MB (11.45%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
+    int solve(int n, vector<int> &dp){
+        if(n <= 1)
+        return n;
+
+        if(dp[n] != -1)
+        return dp[n];
+
+        dp[n] = solve(n-1, dp) + solve(n-2, dp);
+        return dp[n];
+    }
+    
     int fib(int n) {
-        if (n <= 1){
-            return n;
-        }else{
-            return fib(n-1) + fib(n - 2);
-        }
+        vector<int> dp(n+1, -1);
+        return solve(n, dp);
     }
 };
