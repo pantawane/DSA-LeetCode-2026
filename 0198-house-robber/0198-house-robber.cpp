a15aa2deb244f5ba97326fd793e4a47e6793fac9
VANSH MANOJ PANTAWANE <pantawanevansh75@gmail.com>
2026-06-10 14:58:52 +0530

    Time: 0 ms (100%), Space: 10 MB (88.6%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -27,21 +27,38 @@ public:
     //      dp[n] = max(include, exclude);
     //      return dp[n];
     // }
-    int solveTab(vector<int>& nums){
-        int n = nums.size();
-        vector<int> dp(n, 0);
+    // int solveTab(vector<int>& nums){
+    //     int n = nums.size();
+    //     vector<int> dp(n, 0);
 
-        dp[0] = nums[0];
+    //     dp[0] = nums[0];
+
+    //     for(int i=1; i<n; i++){
+    //         int include = nums[i];
+    //         if (i > 1)
+    //           include += dp[i-2] ;
+    //         int exclude = dp[i-1] + 0;
+
+    //         dp[i] = max(include, exclude);
+    //     }
+    //     return dp[n -1];
+    // }
+    int solveSpaceOptimization(vector<int>& nums){
+        int n = nums.size();
+        int prev2 = 0;
+        int prev1 = nums[0];
 
         for(int i=1; i<n; i++){
             int include = nums[i];
             if (i > 1)
-              include += dp[i-2] ;
-            int exclude = dp[i-1] + 0;
+              include += prev2 ;
+            int exclude = prev1 + 0;
 
-            dp[i] = max(include, exclude);
+            int ans = max(include, exclude);
+            prev2 = prev1;
+            prev1 = ans;
         }
-        return dp[n -1];
+        return prev1;
     }
 
     int rob(vector<int>& nums) {
@@ -52,7 +69,9 @@ public:
         // vector<int> dp(n+1, -1);
         // int ans = solveMem(nums, n-1, dp);
         // return ans;
-        return solveTab(nums);
+        //return solveTab(nums);
+
+        return solveSpaceOptimization(nums);
 
     }
 };
