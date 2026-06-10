a47e034d37bce409684e35a0dd439dd9b34d72aa
VANSH MANOJ PANTAWANE <pantawanevansh75@gmail.com>
2026-06-10 08:07:27 +0530

    Time: 0 ms (100%), Space: 97.3 MB (86.78%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
         int minPrice = prices[0];
         int maxProfit = 0;
 
-        for(int i = 0; i< prices.size(); i++){
+        for(int i = 1; i< prices.size(); i++){
             int profit = prices[i] - minPrice;
             maxProfit = max(maxProfit, profit);
             minPrice = min(minPrice, prices[i]);
