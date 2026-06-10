aedd4e038fde74499d32d5ed58c81cf02ebd470b
VANSH MANOJ PANTAWANE <pantawanevansh75@gmail.com>
2026-06-10 14:35:13 +0530

    Time: 0 ms (100%), Space: 10.6 MB (61.17%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    // int solve(vector<int>& nums, int n){
+    //     if(n < 0)
+    //     return 0;
+    //     if(n == 0)
+    //     return nums[0];
+
+    //     int include = solve(nums, n-2) + nums[n];
+    //     int exclude = solve(nums, n-1) + 0;
+        
+    //      return max(include, exclude);
+    // }
+    int solveMem(vector<int>& nums, int n, vector<int> &dp){
+        if(n < 0)
+        return 0;
+        if(n == 0)
+        return nums[0];
+
+        if(dp[n] != -1)
+        return dp[n];
+
+        int include = solveMem(nums, n-2, dp) + nums[n];
+        int exclude = solveMem(nums, n-1, dp) + 0;
+        
+         dp[n] = max(include, exclude);
+         return dp[n];
+    }
+
+    int rob(vector<int>& nums) {
+        // int n = nums.size();
+        // int ans = solve(nums, n-1);
+        // return ans;
+        int n = nums.size();
+        vector<int> dp(n+1, -1);
+        int ans = solveMem(nums, n-1, dp);
+        return ans;
+    }
+};
