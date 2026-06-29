d6f9d3d1b84563dcbbe5581066f04df96d28ef3c
VANSH MANOJ PANTAWANE <pantawanevansh75@gmail.com>
2026-06-29 16:28:23 +0530

    Time: 44 ms (51.02%), Space: 81.7 MB (52.61%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
-    // Custom comparator
-    static bool cmp(vector<int>& a, vector<int>& b) {
-        if (a[0] == b[0])
-            return a[1] > b[1];      // Same width -> height in descending order
+    static bool cmp(vector<int> &a, vector<int> &b){
+        if(a[0] == b[0])
+        return a[1] > b[1];  //same width- height decending order
 
-        return a[0] < b[0];          // Width in ascending order
+        return a[0] < b[0];
     }
 
     int maxEnvelopes(vector<vector<int>>& envelopes) {
-
-        // Step 1: Sort
+        //sort
         sort(envelopes.begin(), envelopes.end(), cmp);
 
-        // Step 2: Apply LIS on heights
+        //using LIS on height only
         vector<int> ans;
 
         ans.push_back(envelopes[0][1]);
-
-        for (int i = 1; i < envelopes.size(); i++) {
-
+        for(int i=1; i<envelopes.size(); i++){
             int height = envelopes[i][1];
 
-            if (height > ans.back()) {
-                ans.push_back(height);
-            }
-            else {
+            if(height > ans.back())
+            ans.push_back(height);
+            else{
                 int index = lower_bound(ans.begin(), ans.end(), height) - ans.begin();
                 ans[index] = height;
             }
         }
 
-        return ans.size();
+       return ans.size();
+
     }
 };
