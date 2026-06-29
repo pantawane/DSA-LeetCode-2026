6d224fb21ef4dfa8f4570cab21e4f21fc7454d40
VANSH MANOJ PANTAWANE <pantawanevansh75@gmail.com>
2026-06-29 13:52:59 +0530

    Time: 507 ms (20.76%), Space: 296.6 MB (6.44%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -32,10 +32,31 @@ public:
         return dp[curr][prev + 1] = max(include, exclude);
     }
 
+    //Tabulation
+    int solveTab(int n, vector<int>& nums, int curr, int prev){
+
+        vector<vector<int>> dp(n + 1, vector<int> (n+1, 0));
+
+       for(curr = n-1; curr >=0; curr--){
+        for(prev = n-1; prev >= -1; prev--){
+            int include = 0;
+    
+            if(prev == -1 || nums[curr] > nums[prev])
+            include = 1 + dp[curr + 1][curr + 1];
+
+            int exclude = 0 + dp[curr+1][prev + 1];
+
+           dp[curr][prev + 1] = max(include, exclude);
+        }
+       }
+       return dp[0][0];
+    }
+
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         //return solve(n, nums, 0, -1);
-        vector<vector<int>> dp(n, vector<int> (n+1, -1));
-        return solveMem(n, nums, 0, -1, dp);
+        // vector<vector<int>> dp(n, vector<int> (n+1, -1));
+        // return solveMem(n, nums, 0, -1, dp);
+        return solveTab(n, nums, 0, -1);
     }
 };
