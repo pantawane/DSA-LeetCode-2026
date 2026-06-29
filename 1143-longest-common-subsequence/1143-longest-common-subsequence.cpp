7dc2e1d582d82e6f6528806699980139d9e8f4c6
VANSH MANOJ PANTAWANE <pantawanevansh75@gmail.com>
2026-06-29 17:26:04 +0530

    Time: 31 ms (36.86%), Space: 45.2 MB (5.82%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -36,9 +36,28 @@ public:
         return dp[i][j] = ans;
     }
 
+    int solveTab(string &a, string &b, int i, int j){
+
+        vector<vector<int>> dp(a.length()+1, vector<int> (b.length()+1, 0));
+
+        for(int i=a.length()-1; i>=0; i--){
+            for(int j= b.length()-1; j>=0; j--){
+            int ans = 0;
+            if(a[i] == b[j]){
+            ans = 1 + dp[i+1][j+1];
+            }else{
+            ans = max(dp[i+1][j], dp[i][j+1]);
+        }
+            dp[i][j] = ans;
+            }
+        }
+        return dp[0][0];
+    }
+
     int longestCommonSubsequence(string text1, string text2) {
         //return solve(text1, text2, 0, 0);
         vector<vector<int>> dp(text1.length(), vector<int> (text2.length(), -1));
-        return solveMem(text1, text2, 0, 0, dp);
+        // return solveMem(text1, text2, 0, 0, dp);
+        return solveTab(text1, text2, 0, 0);
     }
 };
