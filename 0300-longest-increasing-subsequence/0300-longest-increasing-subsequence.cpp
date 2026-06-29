60f9c61cadd09a64d3b723b6ed53fed2e1422cff
VANSH MANOJ PANTAWANE <pantawanevansh75@gmail.com>
2026-06-29 13:25:35 +0530

    Time: 621 ms (5.49%), Space: 296.4 MB (20.01%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    //recursion
+    int solve(int n, vector<int>& nums, int curr, int prev){
+        //base case
+        if(curr == n)
+        return 0;
+
+        int include = 0;
+        if(prev == -1 || nums[curr] > nums[prev])
+        include = 1 + solve(n, nums, curr + 1, curr);
+
+        int exclude = 0 + solve(n, nums, curr + 1, prev);
+
+        return max(include, exclude);
+    }
+    //resursion + memoization
+    int solveMem(int n, vector<int>& nums, int curr, int prev, vector<vector<int>> &dp){
+        //base case
+        if(curr == n)
+        return 0;
+
+        if(dp[curr][prev + 1] != -1)
+        return dp[curr][prev + 1];
+
+        int include = 0;
+        if(prev == -1 || nums[curr] > nums[prev])
+        include = 1 + solveMem(n, nums, curr + 1, curr, dp);
+
+        int exclude = 0 + solveMem(n, nums, curr + 1, prev, dp);
+
+        return dp[curr][prev + 1] = max(include, exclude);
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        //return solve(n, nums, 0, -1);
+        vector<vector<int>> dp(n, vector<int> (n+1, -1));
+        return solveMem(n, nums, 0, -1, dp);
+    }
+};
