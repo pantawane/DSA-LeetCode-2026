e2f6d60389f1d8ead8ba4d3042fc90d3b57e2652
VANSH MANOJ PANTAWANE <pantawanevansh75@gmail.com>
2026-06-30 10:39:10 +0530

    Time: 8 ms (45.63%), Space: 13 MB (72.31%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -38,7 +38,7 @@ public:
 
         int ans = 0;
         if(a[i] == b[j]){
-            return solveMem(a, b, i+1, j+1, dp);
+            return dp[i][j] = solveMem(a, b, i+1, j+1, dp);
         } 
         else{
             //insert
@@ -53,10 +53,44 @@ public:
         return dp[i][j] = ans;
     }
 
+    int solveTab(string &a, string &b){
+        vector<vector<int>> dp(a.size() + 1, vector<int> (b.size() + 1, 0));
+
+        for(int j=0; j<=b.size(); j++){
+            dp[a.size()][j] = b.size() - j;
+        }
+
+        for(int i=0; i<= a.size(); i++){
+            dp[i][b.size()] = a.size() - i;
+        }
+
+        for(int i = a.size()-1; i>=0; i--){
+            for(int j= b.size()-1; j>=0; j--){
+                int ans = 0;
+            if(a[i] == b[j]){
+                ans = dp[i+1][j+1];
+            } 
+            else{
+                //insert
+                int insertAns = 1 + dp[i][j+1];
+                //delete
+                int deleteAns = 1 + dp[i+1][j];
+                //replace
+                int replaceAns = 1 + dp[i+1][j+1];
+
+                ans = min(insertAns, min(deleteAns, replaceAns));
+            }
+             dp[i][j] = ans;
+            }
+        }
+
+        return dp[0][0];
+    }
+
     int minDistance(string word1, string word2) {
         //return solve(word1, word2, 0,0);
-        
-        vector<vector<int>> dp(word1.size(), vector<int> (word2.size(), -1));
-        return solveMem(word1, word2, 0,0, dp);
+        //vector<vector<int>> dp(word1.size(), vector<int> (word2.size(), -1));
+        // return solveMem(word1, word2, 0,0, dp);
+        return solveTab(word1, word2);
     }
 };
