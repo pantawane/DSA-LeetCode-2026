d0173c50c50a30231e1fe7e9fe78e8b1a9a504db
VANSH MANOJ PANTAWANE <pantawanevansh75@gmail.com>
2026-07-02 09:51:21 +0530

    Time: 1 ms (20.27%), Space: 12.8 MB (68.79%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -45,12 +45,35 @@ public:
         }
         return dp[0][0];
     }
+    //space optimization
+    int solveSpaOpt(vector<vector<int>>& triangle){
+        int n = triangle.size();
+
+        vector<int> curr(n+1, 0);
+        vector<int> next(n+1, 0);
+        //base case analyze
+        for(int j=0; j<n; j++){
+            next[j] = triangle[n-1][j];
+        }
+
+        for(int i = n-2; i>=0; i--){
+            for(int j = i; j >= 0; j--){
+                int down = triangle[i][j] + next[j];
+                int diagonal = triangle[i][j] + next[j+1];
+
+                curr[j] = min(down, diagonal);
+            }
+            next = curr;
+        }
+        return next[0];
+    }
 
     int minimumTotal(vector<vector<int>>& triangle) {
         //return solve(0, 0, triangle);
         int n = triangle.size();
         //vector<vector<int>> dp(n, vector<int> (n, -1));
         //return solveMem(0, 0, triangle, dp);
-        return solveTab(triangle);
+        //return solveTab(triangle);
+        return solveSpaOpt(triangle);
     }
 };
