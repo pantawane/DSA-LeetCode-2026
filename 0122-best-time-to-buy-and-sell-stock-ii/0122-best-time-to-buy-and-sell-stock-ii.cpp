8a0f0f302a799734bff59993918f3c31c458da46
VANSH MANOJ PANTAWANE <pantawanevansh75@gmail.com>
2026-07-03 15:08:45 +0530

    Time: 8 ms (35.41%), Space: 24.8 MB (22.2%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -25,10 +25,30 @@ public:
         }
         return dp[index][buy] = profit;
     }
+    //tabulation
+    int solveTab(vector<int>& prices){
+        int n = prices.size();
+
+        vector<vector<int>> dp(prices.size()+1, vector<int> (2, 0));
+
+        //dp[n-1][buy] = 0;
+        for(int index = n-1; index >= 0; index--){
+            for(int buy = 1; buy >=0; buy--){
+                int profit = 0;
+                if(buy){
+                    profit = max( (-prices[index] + dp[index+1][0]), dp[index+1][1]);
+                }else{
+                    profit = max( (prices[index] + dp[index+1][1]), dp[index+1][0]);
+                }
+                dp[index][buy] = profit;
+            }
+        }
+         return dp[0][1];
+    }
 
     int maxProfit(vector<int>& prices) {
-        
-        vector<vector<int>> dp(prices.size(), vector<int> (2, -1));
-        return solveMem(prices, 0, 1, dp);
+        return solveTab(prices);
+        //vector<vector<int>> dp(prices.size(), vector<int> (2, -1));
+        //return solveMem(prices, 0, 1, dp);
     }
 };
