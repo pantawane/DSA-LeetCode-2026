d654f892379d41b0b49e605f95f090b50582fa7e
VANSH MANOJ PANTAWANE <pantawanevansh75@gmail.com>
2026-07-03 15:51:42 +0530

    Time: 3 ms (32.87%), Space: 16.4 MB (22.72%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int solve(vector<int>& prices, int index, int buy){
+        if(index >= prices.size())
+        return 0;
+        int profit = 0;
+        if(buy){
+            profit = max( (-prices[index] + solve(prices, index+1, 0)), solve( prices, index+1, 1));
+        }else{
+            profit = max( (prices[index] + solve(prices, index+2, 1)), solve(prices, index+1, 0));
+        }
+        return profit;
+    }
+     //memoization
+    int solveMem(vector<int>& prices, int index, int buy, vector<vector<int>> &dp){
+        if(index >= prices.size())
+        return 0;
+        if(dp[index][buy] != -1)
+        return dp[index][buy];
+        int profit = 0;
+        if(buy){
+            profit = max( (-prices[index] + solveMem(prices, index+1, 0, dp)), solveMem( prices, index+1, 1, dp));
+        }else{
+            profit = max( (prices[index] + solveMem(prices, index+2, 1, dp)), solveMem(prices, index+1, 0, dp));
+        }
+        return dp[index][buy] = profit;
+    }
+
+    int maxProfit(vector<int>& prices) {
+        // return solve(prices, 0, 1);
+        vector<vector<int>> dp(prices.size(), vector<int> (2, -1));
+        return solveMem(prices, 0, 1, dp);
+    }
+};
