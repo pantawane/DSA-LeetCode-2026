c4007ac0999dfac82db63892c42b3214d308c8fc
VANSH MANOJ PANTAWANE <pantawanevansh75@gmail.com>
2026-07-09 08:20:07 +0530

    Time: 0 ms (100%), Space: 22.1 MB (82.51%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,27 +2,30 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> left(n, 1);
-        vector<int> right(n, 1);
-
-        for(int i=1; i<n; i++){
-            if(ratings[i] > ratings[i-1]){
-                left[i] = left[i-1] + 1;
-            }else{
-                left[i] = 1;
+        int sum = 1; 
+        int i = 1;
+        while(i < n){
+            if(ratings[i] == ratings[i-1]){
+                sum ++;
+                i++;
+                continue;
             }
+        int peak = 1;
+        while(i < n && ratings[i] > ratings[i-1]){
+            peak ++;
+            sum += peak;
+            i++;
         }
-        for(int i= n-2; i>=0; i--){
-            if(ratings[i] > ratings[i+1]){
-                right[i] = right[i+1] + 1;
-            }else{
-                right[i] = 1;
-            }
+        int down = 1;
+        while(i < n && ratings[i] < ratings[i-1]){
+            sum += down;
+            down ++;
+            i++;
         }
-        int sum = 0;
-        for(int i=0; i<n; i++){
-            sum += max(left[i], right[i]);
+        if(down > peak){
+            sum += down - peak;
         }
+    }
         return sum;
     }
 };
