3227bb4b3f696855f4ba1dd80264776376f1f9a7
VANSH MANOJ PANTAWANE <pantawanevansh75@gmail.com>
2026-07-10 13:46:08 +0530

    Time: 6 ms (44.8%), Space: 23.8 MB (59.52%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    static bool comp(vector<int> &a, vector<int> &b){
+        return a[0] < b[0];
+    }
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), comp);
+
+        vector<vector<int>> ans;
+        ans.push_back(intervals[0]);
+
+        for(int i=0; i<intervals.size(); i++){
+            if(intervals[i][0] <= ans.back()[1]){
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            }
+            else{
+                ans.push_back(intervals[i]);
+            }
+        }
+        return ans;
+    }
+};
