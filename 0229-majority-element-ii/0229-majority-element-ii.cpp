0dddc94429b8b9632424bedb3d836d2fab25ddb8
VANSH MANOJ PANTAWANE <pantawanevansh75@gmail.com>
2026-07-15 17:44:02 +0530

    Time: 0 ms (100%), Space: 23.6 MB (65.7%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,16 +3,39 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         vector<int> list;
         int n = nums.size();
-        unordered_map<int, int> mp;
-        int minElement = n / 3 + 1;
+        int cnt1 = 0;
+        int cnt2 = 0;
+        int element1 = INT_MIN;
+        int element2 = INT_MIN;
 
         for(int i=0; i<n; i++){
-            mp[nums[i]]++;
+            if(cnt1 == 0 && nums[i] != element2){
+                cnt1 = 1;
+                element1 = nums[i];
+            }
+            else if(cnt2 == 0 && nums[i] != element1){
+                cnt2 = 1;
+                element2 = nums[i];
+            }
+            else if(element1 == nums[i]) cnt1++;
+            else if(element2 == nums[i]) cnt2++;
 
-            if(mp[nums[i]] == minElement){
-                list.push_back(nums[i]);
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
+        // manual check
+        int count1 = 0;
+        int count2 = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] == element1) count1++;
+            if(nums[i] == element2) count2++;
+        }
+        int mini = (int) n/3 + 1;
+        if(count1 >= mini) list.push_back(element1);
+        if(count2 >= mini) list.push_back(element2);
+
         return list;
     }
 };
