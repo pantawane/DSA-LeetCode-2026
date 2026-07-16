ff59c34ea755e5d3707d87423fb566578e4b0ced
VANSH MANOJ PANTAWANE <pantawanevansh75@gmail.com>
2026-07-16 15:43:01 +0530

    Time: 0 ms (100%), Space: 15.7 MB (73.18%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,7 +1,33 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
-        return;
+        int n = nums.size();
+
+        int index = -1;
+
+        // Step 1: Find pivot
+        for(int i = n - 2; i >= 0; i--) {
+            if(nums[i] < nums[i + 1]) {
+                index = i;
+                break;
+            }
+        }
+
+        // If already the last permutation
+        if(index == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        // Step 2: Find next greater element
+        for(int i = n - 1; i > index; i--) {
+            if(nums[i] > nums[index]) {
+                swap(nums[i], nums[index]);
+                break;
+            }
+        }
+
+        // Step 3: Reverse the suffix
+        reverse(nums.begin() + index + 1, nums.end());
     }
 };
