617e06d693efaec1531c43a628c682eab9656112
VANSH MANOJ PANTAWANE <pantawanevansh75@gmail.com>
2026-07-23 09:00:44 +0530

    Time: 1 ms (72.63%), Space: 8.3 MB (79.76%) - LeetHub

--- 0887-super-egg-drop/0887-super-egg-drop.cpp
+++ 0887-super-egg-drop/0887-super-egg-drop.cpp
@@ -70,23 +70,48 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int superEggDrop(int k, int n) {
+
+//         vector<vector<long long>> dp(n + 1, vector<long long>(k + 1, 0));
+
+//         int moves = 0;
+
+//         while (dp[moves][k] < n) {
+
+//             moves++;
+
+//             for (int eggs = 1; eggs <= k; eggs++) {
+
+//                 dp[moves][eggs] =
+//                     dp[moves - 1][eggs - 1] +
+//                     dp[moves - 1][eggs] +
+//                     1;
+//             }
+//         }
+
+//         return moves;
+//     }
+// };
+
 class Solution {
 public:
     int superEggDrop(int k, int n) {
 
-        vector<vector<long long>> dp(n + 1, vector<long long>(k + 1, 0));
+        vector<long long> dp(k + 1, 0);
 
         int moves = 0;
 
-        while (dp[moves][k] < n) {
+        while (dp[k] < n) {
 
             moves++;
 
-            for (int eggs = 1; eggs <= k; eggs++) {
+            for (int eggs = k; eggs >= 1; eggs--) {
 
-                dp[moves][eggs] =
-                    dp[moves - 1][eggs - 1] +
-                    dp[moves - 1][eggs] +
+                dp[eggs] =
+                    dp[eggs] +
+                    dp[eggs - 1] +
                     1;
             }
         }
