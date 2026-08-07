d2d388840d9fe2b4c523a3a84c8ca52a60721079
VANSH MANOJ PANTAWANE <pantawanevansh75@gmail.com>
2026-08-07 10:22:13 +0530

    Time: 0 ms (100%), Space: 25.9 MB (78.25%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -5,30 +5,30 @@ public:
         if(height.empty()){
             return 0;
         }
-        //size of height
-        int n = height.size()  ;
-
-        //create leftMax and rightMax
-        vector<int> leftMax(n);
-        vector<int> rightMax(n);
-
-        //initialize
-        leftMax[0] = height[0];
-        for(int i=1; i<n; i++){
-            leftMax[i] = max(leftMax[i-1], height[i]);
+        int n = height.size();
+        
+        int l = 0; 
+        int r = n-1;
+        int lmax = 0, rmax=0, total = 0;
+        while(l < r){
+            if(height[l] <= height[r]){
+                if(lmax > height[l]){
+                    total += lmax - height[l];
+                }else{
+                    lmax = height[l];
+                }
+                l = l+1;
+            }
+            else{
+                if(rmax > height[r]){
+                    total += rmax - height[r];
+                }else{
+                    rmax = height[r];
+                }
+                r = r-1;
+            }
         }
 
-        rightMax[n-1] = height[n-1];
-        for(int i=n-2; i>=0; i--){
-            rightMax[i] = max(rightMax[i+1], height[i]);
-        }
-
-        int water = 0;
-        for(int i=0; i<n; i++){
-            water += min(leftMax[i], rightMax[i]) - height[i];
-        }
-
-        return water;
-
+        return total;
     }
 };
