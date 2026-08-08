e8a872af737aa1abd214c60b6d6eaef58495a372
VANSH MANOJ PANTAWANE <pantawanevansh75@gmail.com>
2026-08-08 12:53:50 +0530

    Time: 1 ms (83.69%), Space: 11.5 MB (43.38%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,55 +1,41 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        //base case
-        if(t.size() > s.size())
-        return "";
-        // create need -> what we need from t
-        // create window what we currently have in window
-        unordered_map<char, int> need;
-        unordered_map<char, int> window;
+        int n = s.size();
+        int m = t.size();
+        int startIdx = -1;
+        int minLen = INT_MAX;
+        int cnt = 0;
+        int l=0, r=0;
 
-        for(char ch : t)
-           need[ch]++;
+        vector<int> hash(256, 0);
 
-        //initialize variables
-        int requirement = need.size();
-        int formed = 0;
-        int i = 0;
-        int minLength = INT_MAX;
-        int start = 0;
-
-        //start sliding window
-        for(int j = 0; j< s.size(); j++){
-            char ch = s[j];
-            window[ch]++;
-
-            //check condition
-            if(need.count(ch) && window[ch] == need[ch])
-               formed++;
-
-            while(formed == requirement){
-
-                //update minLength
-                if(j-i+1 < minLength){
-                minLength = j-i+1;
-                start = i;
-            }
-
-            //remove leftChar
-            char leftChar = s[i];
-            window[leftChar]--;
-
-            if(need.count(leftChar) && window[leftChar] < need[leftChar]){
-                formed--;
-            }
-            i++;
-            } 
+        for(char ch: t){
+            hash[ch]++;
         }
-        if(minLength == INT_MAX)
-        return "";
-        
-        return s.substr(start, minLength);
 
+        while(r < n){
+            if(hash[s[r]] > 0){
+                cnt = cnt + 1;
+            }
+
+            hash[s[r]]--;
+
+            while(cnt == m){
+                if(r-l+1 < minLen){
+                    minLen = r-l+1;
+                    startIdx = l;
+                }
+
+                hash[s[l]]++;
+                if(hash[s[l]] > 0) {
+                    cnt = cnt - 1;
+                } 
+                l = l + 1; 
+            }
+            r = r+1;
+        }
+
+        return startIdx == -1 ? "" : s.substr(startIdx, minLen);
     }
 };
