b046d8d0cc74f41b56f7e435d0ade0a24460939a
VANSH MANOJ PANTAWANE <pantawanevansh75@gmail.com>
2026-08-08 08:44:56 +0530

    Time: 191 ms (20.92%), Space: 23.6 MB (10.22%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,35 +1,46 @@
 class Solution {
 public:
-    bool checkInclusion(string s1, string s2) {
-        int n = s1.size();
-        int m = s2.size();
-        //base case
-        if(n > m)
-        return false;
 
-        vector<int> freq1(26,0);
-        vector<int> freq2(26,0);
-
-        for(char ch: s1)
-          freq1[ch - 'a']++;
-
-        for(int i=0; i<n; i++)
-          freq2[s2[i] - 'a']++;
-
-        if(freq1 == freq2)
+    bool isFreqMatches(vector<int>& freq1, vector<int>& freq2) {
+        for (int i = 0; i < 26; i++) {
+            if (freq1[i] != freq2[i]) {
+                return false;
+            }
+        }
         return true;
+    }
 
-        //slide window
-        for(int j = n; j< m; j++){
-            //add next character
-            freq2[s2[j] -'a']++;
+    bool checkInclusion(string s1, string s2) {
 
-            //remove 1st elemnt
-            freq2[s2[j-n] - 'a']--;
+        vector<int> freq(26, 0);
 
-            if(freq1 == freq2)
-            return true;
-        }    
+        // Frequency of s1
+        for (int i = 0; i < s1.length(); i++) {
+            freq[s1[i] - 'a']++;
+        }
+
+        int windowSize = s1.length();
+
+        for (int i = 0; i < s2.length(); i++) {
+
+            int windowIdx = 0;
+            int idx = i;
+
+            vector<int> windowFreq(26, 0);
+
+            while (windowIdx < windowSize && idx < s2.length()) {
+
+                windowFreq[s2[idx] - 'a']++;
+
+                windowIdx++;
+                idx++;
+            }
+
+            if (isFreqMatches(freq, windowFreq)) {
+                return true;
+            }
+        }
+
         return false;
     }
 };
